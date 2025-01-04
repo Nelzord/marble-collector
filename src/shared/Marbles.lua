a0482139ff7a61fbca294510c6901d5c8ee0bc3a
Nelzord <jmn3lson@gmail.com>
2025-01-04 14:35:18 -0600

    add ability to change colors, add ui

---
+++ src/shared/Marbles.lua
@@ -0,0 +1,15 @@
+-- src/shared/Marbles.lua
+return {
+    {
+        Name = "RedNormal",
+        Color = Color3.fromRGB(255, 0, 0)
+    },
+    {
+        Name = "BlueJump",
+        Color = Color3.fromRGB(0, 0, 255)
+    },
+    {
+        Name = "GreenFaster",
+        Color = Color3.fromRGB(0, 255, 0)
+    }
+}
