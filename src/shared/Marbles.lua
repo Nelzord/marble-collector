f91abc1cbdb9c87fe5a99de6320764707926d61d
Nelzord <jmn3lson@gmail.com>
2025-01-04 16:33:18 -0600

    add marble unlocking

--- src/shared/Marbles.lua
+++ src/shared/Marbles.lua
@@ -1,15 +1,28 @@
 -- src/shared/Marbles.lua
 return {
     {
+        LevelUnlocked = "X",
+        Name = "DefaultMarble",
+        Color = Color3.fromRGB(255, 0, 0),
+        Description = "Just your ordinary marble"
+    },
+    {
+        LevelUnlocked = "Level1Plate",
         Name = "RedNormal",
-        Color = Color3.fromRGB(255, 0, 0)
+        Color = Color3.fromRGB(255, 0, 0),
+        Description = "Red marble description"
     },
     {
+        LevelUnlocked = "Level2Plate",
         Name = "BlueJump",
-        Color = Color3.fromRGB(0, 0, 255)
+        Color = Color3.fromRGB(0, 0, 255),
+        Description = "This marble got a bit of jump to it"
     },
     {
-        Name = "GreenFaster",
-        Color = Color3.fromRGB(0, 255, 0)
+        LevelUnlocked = 3,
+        Name = "Level3Plate",
+        Color = Color3.fromRGB(0, 255, 0),
+        Description = "This marble is speedy"
     }
+    
 }
