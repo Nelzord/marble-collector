67d592b43fe8105219b34eb1f90c86a3131ee6e4
Nelzord <jmn3lson@gmail.com>
2025-01-05 12:58:13 -0600

    add marble name fix, add first unlock notif

--- src/shared/Marbles.lua
+++ src/shared/Marbles.lua
@@ -2,25 +2,25 @@
 return {
     {
         LevelUnlocked = "X",
-        Name = "DefaultMarble",
-        Color = Color3.fromRGB(255, 0, 0),
+        Name = "Default Marble",
+        Color = Color3.fromRGB(50, 50, 50),
         Description = "Just your ordinary marble"
     },
     {
         LevelUnlocked = "Level1Plate",
-        Name = "RedNormal",
+        Name = "Speedy Red",
         Color = Color3.fromRGB(255, 0, 0),
         Description = "Red marble description"
     },
     {
         LevelUnlocked = "Level2Plate",
-        Name = "BlueJump",
+        Name = "Blue Jump",
         Color = Color3.fromRGB(0, 0, 255),
         Description = "This marble got a bit of jump to it"
     },
     {
-        LevelUnlocked = 3,
-        Name = "Level3Plate",
+        LevelUnlocked = "Level3Plate",
+        Name = "Green Gravity",
         Color = Color3.fromRGB(0, 255, 0),
         Description = "This marble is speedy"
     }
